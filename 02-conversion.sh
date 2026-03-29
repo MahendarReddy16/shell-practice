41dd70d0368094a62a6e936729bf1b03f29233cd
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-03-29 18:39:34 +0530

    conversion of both

---
+++ 02-conversion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "mahi:: Hi maha, how are you?"
+echo "maha:: Hi mahi, I'm fine, how are you?"
+echo "mahi:: I'm doing good. What's going on?"
+echo "maha:: I started learning shell scripting"
+echo "mahi:: Intersting, what is the lastest on you have learned"
+echo "maha:: Just started"
