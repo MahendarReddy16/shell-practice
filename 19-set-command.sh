23208725d4c7f460c51e7e6f3fbd6d2336f02875
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-04-22 12:46:55 +0530

    check

---
+++ 19-set-command.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#It terminates the execution when the error occurs.
+set -e
+
+echo "Hello World"
+doo
+echo Mahi
+
+#We can see that the 7th line is not getting printed as the execution got terminated after the 6th line.
+
+# -ex for the debug
+set -ex
+
+echo "Hello World"
+doo
+echo Mahi
+
