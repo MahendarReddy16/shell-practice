fb798fd8d35836a3f970294bf2422ebda8c481ac
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-04-22 12:59:38 +0530

    check

--- 19-set-command.sh
+++ 19-set-command.sh
@@ -15,3 +15,17 @@ set -ex
 echo "Hello World"
 echo Mahi
 
+
+####
+
+set -e # setting the automatic exit, if we get error, set -ex for debug
+
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
+
+echo "Hello World success"
+echooooo "Hello Wolrd failure"
+echo "Hello World after failure"
