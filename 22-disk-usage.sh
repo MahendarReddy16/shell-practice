38a5e0a3a977ef438c409638d62caac5be11bcaa
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-04-24 17:32:09 +0530

    logs

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep Xfs)
 DISK_THRESHOLD=
 
-while IFS=read -r line
+while IFS= read -r line
 do
 USAGE=$(echo $line | grep Xfs | awk -F " " {print $6F} | cut -d "%" -f1)
 PARTITION=$(echo $line | grep Xfs | awk -F " " {print $NF})
