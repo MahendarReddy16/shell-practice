9e890e085a2d9a4b589ad4391260d159cec096c4
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-04-24 17:49:26 +0530

    logs

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -4,13 +4,13 @@ DISK_USAGE=$(df -hT | grep Xfs)
 DISK_THRESHOLD=5
 
 while IFS= read -r line
-do
-USAGE=$(echo $line | grep Xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-echo "$USAGE"
-PARTITION=$(echo $line | grep Xfs | awk -F " " '{print $NF}')
-echo "$PARITION"
-if [ $USAGE -gt $DISK_THRESHOLD ]
-then 
-    echo "$PARTITION is greater than the $DISK_THRESHOLD, current value $USAGE, please check"
-fi
+do    
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    echo "$USAGE"
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    echo "$PARITION"
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then 
+        echo "$PARTITION is greater than the $DISK_THRESHOLD, current value $USAGE, please check"
+    fi
 done <<< $DISK_USAGE
